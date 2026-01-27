1f8ee0e09f69c9a65fd4ac7699ecb15c9df3eedc
EricXu404 <xuec@mail.uc.edu>
2026-01-26 19:45:21 -0500

    Forgot some formatting for constant values

--- Algorithm_Analysis_Task_2.cpp
+++ Algorithm_Analysis_Task_2.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 string convertBase(int p, int b) {
     // Assignment words it weird, but my best guess is that A=10, B=11, C=12,... etc. where p=14 and b=15 should give you E which is 14 in decimal
-    string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";  // The decimal to base 10-36 dictionary
+    string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";  // The decimal to base 10-36 dictionary
     string result = "";
 
     while (p > 0) {
         // Basically a dictionary look up using a string and indexes and appends them to the result string
-        result = digits[p % b] + result;
+        result = DIGITS[p % b] + result;
         p /= b;
     }
     return result;
